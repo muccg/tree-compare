fc6dbefa71b23993b0d9d313d7ab1c4c871ab9db
Grahame Bowland <grahame@users.noreply.github.com>
2013-08-30 16:21:56 +0800

    scan wheat

--- legacy-to-bpaarchive/compare.sh
+++ legacy-to-bpaarchive/compare.sh
@@ -20,3 +20,21 @@ cd ~/tree-compare/legacy-to-bpaarchive || exit 1
         /pbstore/groupfs/bpa/legacy/melanoma/raw/agrf/ \
         /pbstore/groupfs/bpa/bpaarchive/Melanoma/raw/AGRF/
 
+# wheat / cultivar
+~/tree-compare/tree-compare.py \
+        -c treecompcache.json \
+        -l wheat_cultivar.csv \
+        -t /pbstore/groupfs/bpa/bpaarchive/Wheat_Cultivars/XXFIXME/ \
+        -x wheat_cultivar.sh \
+        /pbstore/groupfs/bpa/legacy/wheat/cultivar/ \
+        /pbstore/groupfs/bpa/bpaarchive/Wheat_Cultivars/
+
+# wheat / pathogen
+~/tree-compare/tree-compare.py \
+        -c treecompcache.json \
+        -l wheat_pathogen.csv \
+        -t /pbstore/groupfs/bpa/bpaarchive/Wheat_Pathogens/XXFIXME/ \
+        -x wheat_pathogen.sh \
+        /pbstore/groupfs/bpa/legacy/wheat/pathogen/ \
+        /pbstore/groupfs/bpa/bpaarchive/Wheat_Pathogens/
+
